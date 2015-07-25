a2043849fcc4969ad231be201d811d25d6aef473
Mingming Wang <mingming.wang@grabtaxi.com>
2015-07-25 23:33:00 +0800

    Create cut-rod.c

---
+++ dp/cut-rod.c
@@ -0,0 +1,5 @@
+/*
+Given a price table for rod with different length and the lengh of a rod, return the max value that can be get from cutting 
+the rod in pieces or no cutting at all if the price as a whole is the max. The cutting is free.
+Source: Introduction to Algorithms, 3e
+*/
