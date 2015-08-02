56a51a8ef6de5895402bcca8902f109eeeb4e0f9
Mingming Wang <mingming.wang@grabtaxi.com>
2015-08-02 15:50:48 +0000

    add fibonacci dp

---
+++ dp/fibonacci.c
@@ -0,0 +1,36 @@
+/*
+Print Fibonacci using DP
+*/
+#include <stdio.h>
+// index start from 1
+void print_fibonacci(int index);
+
+int main(){
+    
+    print_fibonacci (10);
+}
+void print_fibonacci(int index){
+    
+    if (index <= 0){
+        return;
+    }
+    int resultA, resultB;
+    resultA = 0;
+    printf ("%d",resultA);
+    if (index <=1){
+        return;
+    }
+    resultB = 1;
+    printf (" ,%d",resultB);
+    if (index <=2){
+        return;
+    }
+    for (int i = 3; i <= index; i++) {
+        
+        int current = resultA + resultB;
+        printf (" ,%d", current);
+        resultA = resultB;
+        resultB = current;
+    }
+    printf ("\n");
+}
