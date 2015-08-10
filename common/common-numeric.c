ccd1b8ec18097adcaba023916d9411bf9440d3ea
Mingming Wang <mingming.wang@grabtaxi.com>
2015-08-10 16:15:20 +0800

    add min common function

--- common/common-numeric.c
+++ common/common-numeric.c
@@ -3,3 +3,9 @@ int max(int num1, int num2)
   return num1 > num2 ? num1 : num2;
   
 }
+
+int min(int num1, int num2) 
+{
+  return num1 < num2 ? num1 : num2;
+  
+}
--- common/common-numeric.h
+++ common/common-numeric.h
@@ -2,5 +2,5 @@
 #define COMMON_NUMERIC
 
 int max(int num1, int num2);
-
+int min(int num1, int num2);
 #endif
